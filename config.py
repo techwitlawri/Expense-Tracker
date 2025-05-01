6468a85d1f394f0fb6a1ff8cc4d61a8f24059ded
mmachi <mmachilawri@gmail.com>
2025-05-01 21:26:18 +0100

    made adjustment

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 #  Configuration setting
 
-# config.py
+
 
 import os
 
 class Config:
     # Secret key for session management
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key')
 
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///local.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications for performance
